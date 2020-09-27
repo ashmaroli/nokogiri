68bf3427ed9263169ae3d90907dbc2ed964d1cf1
Ashwin Maroli <ashmaroli@gmail.com>
2020-09-27 23:23:35 +0530

    Encapsulate task entirely

--- tasks/memprof.rb
+++ tasks/memprof.rb
@@ -1,14 +1,14 @@
 # frozen_string_literal: true
 
-require "memory_profiler"
-require "nokogiri"
-
-puts "Testing with Nokogiri v" + Nokogiri::VERSION.to_s
-puts
-
-CONTENTS = File.read(File.expand_path("../test/files/fixture.html", __dir__))
-
 task :profile_memory do
+  require "memory_profiler"
+  require "nokogiri"
+
+  puts "Testing with Nokogiri v" + Nokogiri::VERSION.to_s
+  puts
+
+  CONTENTS = File.read(File.expand_path("../test/files/fixture.html", __dir__))
+
   MemoryProfiler.report do
     1000.times { Nokogiri::HTML::DocumentFragment.parse(CONTENTS).to_html }
   end.pretty_print(scale_bytes: true, normalize_paths: true)
