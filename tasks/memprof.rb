5cff9341a553718e6d913c3302d36376bbbfd26a
Ashwin Maroli <ashmaroli@gmail.com>
2020-09-28 23:04:37 +0530

    Load Nokogiri without a gemspec

--- tasks/memprof.rb
+++ tasks/memprof.rb
@@ -2,20 +2,15 @@
 
 task :profile_memory do
   require "memory_profiler"
-  require "nokogiri"
+  require File.expand_path("../lib/nokogiri", __dir__)
 
   puts "Testing with Nokogiri v" + Nokogiri::VERSION.to_s
   puts
 
   CONTENTS = File.read(File.expand_path("../test/files/fixture.html", __dir__))
-  NOKOGIRI_SERIALIZE_OPTIONS = {
-    :encoding  => 'UTF-8',
-    :save_with => Nokogiri::XML::Node::SaveOptions::DEFAULT_HTML
-  }.freeze
-
   MemoryProfiler.report do
     1000.times do
-      Nokogiri::HTML::DocumentFragment.parse(CONTENTS).to_html(NOKOGIRI_SERIALIZE_OPTIONS)
+      Nokogiri::HTML::DocumentFragment.parse(CONTENTS).to_html
     end
   end.pretty_print(scale_bytes: true, normalize_paths: true)
 end
