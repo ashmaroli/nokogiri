26c2b32b77e79a06e61ed2ead251e96f3374bb54
Ashwin Maroli <ashmaroli@gmail.com>
2020-09-30 12:20:49 +0530

    Extract profiling logic into a module

--- tasks/memprof.rb
+++ tasks/memprof.rb
@@ -2,32 +2,33 @@
 
 namespace :memprof do
   require "memory_profiler"
+  module NokoMemProf
+    def self.profile abs_path
+      puts "\nTesting with Nokogiri v" + Nokogiri::VERSION.to_s
+      puts
+      contents = File.read abs_path
+      MemoryProfiler.report do
+        1000.times { yield contents  }
+      end.pretty_print(scale_bytes: true, normalize_paths: true)
+    end
+  end
+
 
   task :html_doc do
     require "nokogiri"
 
-    puts "\nTesting with Nokogiri v" + Nokogiri::VERSION.to_s
-    puts
-
-    contents = File.read(File.expand_path("../test/files/fixture.html", __dir__))
-    MemoryProfiler.report do
-      1000.times do
-        Nokogiri::HTML::Document.parse(contents).to_html
-      end
-    end.pretty_print(scale_bytes: true, normalize_paths: true)
+    abs_path = File.expand_path("../test/files/fixture.html", __dir__)
+    NokoMemProf.profile(abs_path) do |contents|
+      Nokogiri::HTML::Document.parse(contents).to_html
+    end
   end
 
   task :html_doc_frag do
     require "nokogiri"
 
-    puts "\nTesting with Nokogiri v" + Nokogiri::VERSION.to_s
-    puts
-
-    contents = File.read(File.expand_path("../test/files/fixture_body.html", __dir__))
-    MemoryProfiler.report do
-      1000.times do
-        Nokogiri::HTML::DocumentFragment.parse(contents).to_html
-      end
-    end.pretty_print(scale_bytes: true, normalize_paths: true)
+    abs_path = File.expand_path("../test/files/fixture_body.html", __dir__)
+    NokoMemProf.profile(abs_path) do |contents|
+      Nokogiri::HTML::DocumentFragment.parse(contents).to_html
+    end
   end
 end
