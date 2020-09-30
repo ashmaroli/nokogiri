2973769761259e4e033ae0ef9fabf428324b24bc
Ashwin Maroli <ashmaroli@gmail.com>
2020-09-30 11:43:42 +0530

    Profile processing of document fragment

--- tasks/memprof.rb
+++ tasks/memprof.rb
@@ -1,16 +1,33 @@
 # frozen_string_literal: true
 
-task :profile_memory do
+namespace :memprof do
   require "memory_profiler"
-  require "nokogiri"
 
-  puts "Testing with Nokogiri v" + Nokogiri::VERSION.to_s
-  puts
+  task :html_doc do
+    require "nokogiri"
 
-  CONTENTS = File.read(File.expand_path("../test/files/fixture.html", __dir__))
-  MemoryProfiler.report do
-    1000.times do
-      Nokogiri::HTML::DocumentFragment.parse(CONTENTS).to_html
-    end
-  end.pretty_print(scale_bytes: true, normalize_paths: true)
+    puts "\nTesting with Nokogiri v" + Nokogiri::VERSION.to_s
+    puts
+
+    contents = File.read(File.expand_path("../test/files/fixture.html", __dir__))
+    MemoryProfiler.report do
+      1000.times do
+        Nokogiri::HTML::Document.parse(contents).to_html
+      end
+    end.pretty_print(scale_bytes: true, normalize_paths: true)
+  end
+
+  task :html_doc_frag do
+    require "nokogiri"
+
+    puts "\nTesting with Nokogiri v" + Nokogiri::VERSION.to_s
+    puts
+
+    contents = File.read(File.expand_path("../test/files/fixture_body.html", __dir__))
+    MemoryProfiler.report do
+      1000.times do
+        Nokogiri::HTML::DocumentFragment.parse(contents).to_html
+      end
+    end.pretty_print(scale_bytes: true, normalize_paths: true)
+  end
 end
